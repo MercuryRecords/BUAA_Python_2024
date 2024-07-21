19450cfad01fe712506649672802c251a1291ae5
July <22371500@buaa.edu.cn>
2024-07-21 16:05:55 +0800

    对应接口文档的注册

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,51 +4,62 @@ import axios from 'axios'
 import {reactive, ref} from "vue"
 import API from "@/plugins/axios"
 
-const inputValue = ref('')
+const username = ref('')
+const password = ref('')
+const usertype = ref('')
 const data = reactive(
     {
-      message: ''
+      username: '',
+      password: '',
+      usertype: '',
+    }
+)
+const returnValue = reactive(
+    {
+      code: '',
+      message: '',
     }
 )
 
-function sendMessage() {
-  console.log('hello,world')
-  /*axios.post('/api/register/',data).then(
-      function(){
-        console.log("successfully registered")
-      })
-      .catch(
-          function(){
-            console.log("error!")
-          })*/
-  data.message = inputValue.value
-  console.log(data.message)
-  API.post('/message', data,
+function userRegister() {
+  data.username = username.value
+  data.password = password.value
+  data.usertype = usertype.value
+  console.log(data)
+  API.post('/user_register/', data,
       {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
+        // headers: {
+        //   'Content-Type': 'application/x-www-form-urlencoded'
+        // }
       }).then(
-      function () {
+      function (response) {
+        returnValue.code = response.data.code
+        returnValue.message = response.data.message
         console.log("successfully registered")
+        alert(returnValue.message)
       })
       .catch(
           function () {
             console.log("error!")
           })
-  inputValue.value = ''
+  username.value = ''
+  password.value = ''
+  usertype.value = ''
 }
 </script>
 
 <template>
-  <div class="navigate">
-    <!--    <RouterLink to="/home">首页</RouterLink>-->
-    <input v-model="inputValue" type="text">
-    <button @click="sendMessage()">sendMessage</button>
-  </div>
-
-  <div class="main-content">
-    <RouterView></RouterView>
+  <div>
+    <div>
+      <input type="text" class="username" v-model="username" placeholder="请输入用户名">
+    </div>
+    <div>
+      <input type="password" class="password" v-model="password" placeholder="请输入密码">
+    </div>
+    <div>
+      <input type="text" class="usertype" v-model="usertype" placeholder="请输入用户类型">
+    </div>
+    <button @click="userRegister">注册</button>
   </div>
 </template>
 
