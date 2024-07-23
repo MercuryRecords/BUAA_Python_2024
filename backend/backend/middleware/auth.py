32d17d1b3f1caeefb8affe89ccf4770deeb56e6a
李泰川 <21371285@buaa.edu.cn>
2024-07-23 23:14:48 +0800

    [fix] Authentication

--- backend/backend/middleware/auth.py
+++ backend/backend/middleware/auth.py
@@ -1,14 +1,15 @@
 from django.utils.deprecation import MiddlewareMixin
 from django.http import JsonResponse
 
+
 class Authentication(MiddlewareMixin):
     def process_request(self, request):
         if request.method != "POST":
             return JsonResponse({"code": 400, "message": "请求方式错误"})
-        
-        if request.path_info in ['/api/user_register/', '/api/user_login/']:
+
+        if request.path_info in ['/api/user_register', '/api/user_login', '/api/user_register/', '/api/user_login/']:
             return
-        
+
         register_name = request.session.get("username")
         user_name = request.POST.get("username")
 
@@ -16,4 +17,3 @@ class Authentication(MiddlewareMixin):
         if not register_name or register_name != user_name:
             # 注掉 return 可禁用用户认证
             return JsonResponse({"code": 400, "message": "请先登录"})
-        
