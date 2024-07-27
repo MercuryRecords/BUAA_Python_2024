31f7e32c2f2cc606096f3b2e6ca23662cb91423e
郭永康 <22371214@buaa.edu.cn>
2024-07-27 11:27:08 +0800

    小改格式

--- frontend/src/components/Base/CreateSheet.vue
+++ frontend/src/components/Base/CreateSheet.vue
@@ -278,6 +278,7 @@ const deleteProblemGroup = async (problem: any) => {
       </el-table>
 
       <el-pagination
+          class="pages"
           @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
           :current-page="currentPage"
@@ -415,5 +416,9 @@ const deleteProblemGroup = async (problem: any) => {
 
 .button-container .el-button:last-child {
   margin-right: 0;
+}
+
+.pages {
+  margin-top: 10px;
 }
 </style>
